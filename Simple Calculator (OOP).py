c40fcd97ff296fb01bcde8ee91f9a163630c1b11
Lazaro Baltazary <lazarobaltazary2@gmail.com>
2025-05-18 23:07:32 +0300

    Update Simple Calculator (OOP).py

--- Simple Calculator (OOP).py
+++ Simple Calculator (OOP).py
@@ -8,3 +8,15 @@ class Calculator:
     def add(self):
         return self.a + self.b
 
+    def subtract(self):
+        return self.a - self.b
+
+    def multiply(self):
+        return self.a * self.b
+
+    def divide(self):
+        if self.b != 0:
+            return self.a / self.b
+        else:
+            return "Error: Division by zero"
+
